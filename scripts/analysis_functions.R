3fc8e98cfa4b604b2510b92b3cc47a00cac86335
James Chirombo <jchirombo@mlw.mw>
2023-12-12 20:52:42 +0200

    add new functions

--- scripts/analysis_functions.R
+++ scripts/analysis_functions.R
@@ -1,9 +1,9 @@
 # functions for data analysis
 # create a function for the figures
 # district level prevalence
-plot_district_prevalence <- function(summary.district.poly,lake.poly){
+plot_district_prevalence <- function(summary.district.poly,lake.poly) {
   malawi_prev <- tm_shape(summary.district.poly) +
-    tm_polygons(col = "syph_prev",border.col = "black",title = "Prevalence", palette = "YlGnBu") +
+    tm_polygons(col = "syph_prev",border.col = "black",title = "Prevalence (%)", palette = "YlGnBu") +
     tm_facets(by = "year",ncol = 5, nrow = 2) +
     tm_shape(lake.poly) +
     tm_polygons(col = "lightblue", border.col = "black") +
@@ -18,9 +18,9 @@ plot_district_prevalence <- function(summary.district.poly,lake.poly){
 }
 
 # district level smr
-plot_district_smr <- function(summary.district.poly,lake.poly){
+plot_district_smr <- function(summary.district.poly,lake.poly) {
   malawi_smr <- tm_shape(summary.district.poly) +
-    tm_polygons(col = "smr",border.col = "black", title = "SMR", palette = "YlGnBu") +
+    tm_polygons(col = "smr",border.col = "black", title = "SIR", palette = "YlGnBu") +
     tm_facets(by = "year", ncol = 5, nrow = 2) +
     tm_shape(lake.poly) +
     tm_polygons(col = "lightblue", border.col = "black") +
@@ -34,7 +34,7 @@ plot_district_smr <- function(summary.district.poly,lake.poly){
 }
 
 # exceedance probabilities
-plot_exceedance_probabilities <- function(summary.district.poly,lake.poly){
+plot_exceedance_probabilities <- function(summary.district.poly,lake.poly) {
   exprob <- tm_shape(summary.district.poly) +
     tm_polygons(border.col = "black",col = "exprob", title = "Exceedance prob.", palette = "YlGnBu") +
     tm_facets(by = "year", nrow = 2, ncol = 5) +
@@ -49,9 +49,9 @@ plot_exceedance_probabilities <- function(summary.district.poly,lake.poly){
   return(exprob)
 }
 
-plot_predicted_irr <- function(summary.district.poly, lake.poly, break.style){
+plot_predicted_irr <- function(summary.district.poly, lake.poly, break.style) {
   irr_yr <- tm_shape(summary.district.poly) +
-    tm_polygons(border.col = "black",col = "irr", title = "IRR", n = 7, style = break.style, palette = "YlGnBu") +
+    tm_polygons(border.col = "black",col = "irr", title = "PR", n = 7, style = break.style, palette = "YlGnBu") +
     tm_facets(by = c("year"), nrow = 2, ncol = 5) +
     tm_shape(lake.poly) +
     tm_polygons(col = "lightblue", border.col = "black") +
@@ -63,22 +63,9 @@ plot_predicted_irr <- function(summary.district.poly, lake.poly, break.style){
               panel.label.size = 1.3,
               panel.label.fontface = "bold")
 }
-# function to add denoninator - women of child bearing age
-split_data_by_district <- function(syphilis_data,district_code,pop_vec){
-  df <- filter(syphilis_data,distcode == district_code)
-  df$women_child_age <- rep(pop_vec,each = 12)
-  return(df)
-}
-
-# function to capitalize first letter of a character
-simpleCap <- function(x){
-  s <- strsplit(x, " ")[[1]]
-  paste(toupper(substring(s,1,1)), substring(s,2),
-        sep = "", collapse = " ")
-}
 
 # function to plot ecdf
-plot_ecdf_diagnostics <- function(modelFit,varpos,midpt,nSample){
+plot_ecdf_diagnostics <- function(modelFit,varpos,midpt,nSample) {
   ecdfLower <- ecdf(modelFit$samples$beta[,varpos][1:midpt])
   ecdfUpper <- ecdf(modelFit$samples$beta[,varpos][midpt + 1:nSample])
   plot(ecdfLower,col = "black",lwd = 2)
@@ -92,7 +79,7 @@ plot_ecdf_diagnostics <- function(modelFit,varpos,midpt,nSample){
 }
 
 # function to create and summarize yearly exceedance probabilities
-yearly_posterior_exceedance_prob <- function(year,prob_threshold = 1){
+yearly_posterior_exceedance_prob <- function(year,prob_threshold = 1) {
   estim_risk_samples <- risk_samples_combined[,finaldata$year == year]
   risk_estim_year <- apply(estim_risk_samples,2,median)
   pep_estim_year <- apply(estim_risk_samples > prob_threshold,2,mean)
